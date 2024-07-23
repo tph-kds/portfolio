d61950013fb8ba4b4ec088a05b738d8159613289
tph-kds <hungcompo123@gmail.com>
2024-07-23 11:30:13 +0700

    fix: update error for this file

--- src/components/effect/NavBarEffect.tsx
+++ src/components/effect/NavBarEffect.tsx
@@ -11,7 +11,7 @@ interface NavBarEffectProps {
     navLinks: navLinks[];
   }
 
-const NavBarEffect: React.FC<NavBarEffectProps> = (navLinks:navLinks[]) => {
+const NavBarEffect: React.FC<NavBarEffectProps> = ({navLinks}) => {
 
     
   return (
