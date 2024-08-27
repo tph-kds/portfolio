e3dc525261599053a08d52ec3b088a645fb41ec2
tph-kds <hungcompo123@gmail.com>
2024-08-27 18:38:49 +0700

    fix: enhace and completely deploy on prodcution

--- src/contants/contants.ts
+++ src/contants/contants.ts
@@ -137,28 +137,28 @@ type Props = {
 export const Props: Props[] = [
   {
     title: "Board Game Recommendation System",
-    src: "../../public/assets/logo.png",
+    src: logo,
     description:
       "Designed and implemented a sophisticated recommendation system for board games, combining content-based and collaborative filtering techniques. Implemented a web application using Flask and deployed it on local server. The system merges weighted ratings and averages to deliver personalized suggestions, enhancing user experience by recommending", //The system merges weighted ratings and averages to deliver personalized suggestions, enhancing user experience by recommending games that align with individual preferences and community trends. Modular training and deployment pipelines ensure scalability and adaptability for ongoing improvements.
     link: "https://github.com",
     },
   {
     title: "Visual Question Answering System",
-    src: "../../public/assets/logo.png",
+    src: logo,
     description:
       "Developed an advanced Visual Question Answering system that integrates BERT for natural language processing and Vision Transformer (ViT) for image analysis. Implemented a web application using Streamlit and deployed it on local server.", //This AI-driven system interprets and answers questions based on visual inputs, achieving high accuracy by leveraging the strengths of both models in understanding context and visual content.
     link: "https://github.com",
   },
   {
     title: "TriModal Retrieval Argument Generation System",
-    src: "../../public/assets/logo.png",
+    src: logo,
     description:
       "Developed a Trimodal Retrieval-Augmented Generation (RAG) system that integrates text, image, and audio modalities to enhance content understanding and response generation.", //The system combines advanced retrieval techniques with generative models, delivering contextually rich and accurate outputs across multiple data types.
     link: "https://github.com",
   },
   {
     title: "Blogging Platform",
-    src: "../../public/assets/logo.png",
+    src: logo,
     description:
       "Built a dynamic and responsive blog platform using React and TypeScript. The platform offers a seamless user experience with features like real-time content updates, intuitive navigation, and customizable themes.", // Designed with scalability and maintainability in mind, the project showcases a modern approach to web development with strong type safety and component-based architecture.
     link: "https://github.com",
