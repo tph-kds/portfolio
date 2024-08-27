8b26f9ed1e66ec678a2c01d8b0349d7238271751
tph-kds <hungcompo123@gmail.com>
2024-08-27 19:02:09 +0700

    fix: enhace and completely deploy on prodcution

--- src/contants/contants.ts
+++ src/contants/contants.ts
@@ -140,21 +140,21 @@ export const Props: Props[] = [
     src: logo,
     description:
       "Designed and implemented a sophisticated recommendation system for board games, combining content-based and collaborative filtering techniques. Implemented a web application using Flask and deployed it on local server. The system merges weighted ratings and averages to deliver personalized suggestions, enhancing user experience by recommending", //The system merges weighted ratings and averages to deliver personalized suggestions, enhancing user experience by recommending games that align with individual preferences and community trends. Modular training and deployment pipelines ensure scalability and adaptability for ongoing improvements.
-    link: "https://github.com",
+    link: "https://github.com/tph-kds/recommendation_system_fullstack",
     },
   {
     title: "Visual Question Answering System",
     src: logo,
     description:
       "Developed an advanced Visual Question Answering system that integrates BERT for natural language processing and Vision Transformer (ViT) for image analysis. Implemented a web application using Streamlit and deployed it on local server.", //This AI-driven system interprets and answers questions based on visual inputs, achieving high accuracy by leveraging the strengths of both models in understanding context and visual content.
-    link: "https://github.com",
+    link: "https://github.com/tph-kds/vqa-llm",
   },
   {
     title: "TriModal Retrieval Argument Generation System",
     src: logo,
     description:
       "Developed a Trimodal Retrieval-Augmented Generation (RAG) system that integrates text, image, and audio modalities to enhance content understanding and response generation.", //The system combines advanced retrieval techniques with generative models, delivering contextually rich and accurate outputs across multiple data types.
-    link: "https://github.com",
+    link: "https://github.com/tph-kds/TriModalRAG_System",
   },
   {
     title: "Blogging Platform",
